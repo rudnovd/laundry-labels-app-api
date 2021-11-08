fbd2b2e3c40cd20d1df8b47e2ed1a73bd8cfdea3
rudnovd <rudnov.d@gmail.com>
2021-11-09 00:59:35 +0500

    fix(errors): fix error handler

--- error.ts
+++ error.ts
@@ -1,13 +1,15 @@
 export class AppError extends Error {
   public readonly name: string
   public readonly status: number
+  public readonly message: string
 
-  constructor(name: string, status: number, description: string) {
-    super(description)
+  constructor(name: string, status: number, message: string) {
+    super()
     Object.setPrototypeOf(this, new.target.prototype)
 
     this.name = name
     this.status = status
+    this.message = message
 
     Error.captureStackTrace(this)
   }
