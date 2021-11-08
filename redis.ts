148d4a175b46b2b2a0538ee87c4a2fbbcc2603c3
rudnovd <rudnov.d@gmail.com>
2021-11-09 01:26:36 +0500

    refactor(redis): quit on handle error, start cron if connected

--- redis.ts
+++ redis.ts
@@ -2,25 +2,32 @@ import { CronJob } from 'cron'
 import { createClient } from 'redis'
 
 export const redis = createClient()
-redis.on('error', (err) => console.log('Redis Client Error', err))
+redis.on('error', async (err) => {
+  console.log('Redis Client - ', err)
+  await redis.quit()
+})
 redis.connect().then(() => console.log('connected to Redis'))
 
-const redisCleanImagesJob = new CronJob(
-  '* 30 * * * *',
-  async () => {
-    console.log('job redisCleanImagesJob is running')
-    const keys = await redis.keys('*')
-    const values = keys.map((key) => redis.get(key))
-    Promise.all(values).then((values) => {
-      values.forEach((value, index) => {
-        if (value && Date.now() > parseInt(value)) {
-          redis.del(keys[index])
-        }
+redis.clientId().then((clientId) => {
+  if (!clientId) return
+
+  const redisCleanImagesJob = new CronJob(
+    '* 30 * * * *',
+    async () => {
+      console.log('job redisCleanImagesJob is running')
+      const keys = await redis.keys('*')
+      const values = keys.map((key) => redis.get(key))
+      Promise.all(values).then((values) => {
+        values.forEach((value, index) => {
+          if (value && Date.now() > parseInt(value)) {
+            redis.del(keys[index])
+          }
+        })
       })
-    })
-  },
-  null,
-  true
-)
-redisCleanImagesJob.start()
-console.log('active jobs: redisCleanImagesJob')
+    },
+    null,
+    true
+  )
+  redisCleanImagesJob.start()
+  console.log('active jobs: redisCleanImagesJob')
+})
