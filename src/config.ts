0757c818a3f49453ef4d936238f6469d85448a4a
rudnovd <rudnov.d@gmail.com>
2023-01-15 01:37:32 +0500

    refactor(config): madd cloudinary envs in config

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,9 @@ dotenv.config()
 export const config = {
   jwtSecret: process.env.JWT_SECRET || randomUUID(),
   databaseURI: process.env.DATABASE_URI || '',
+  cloudinary: {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  },
 }
