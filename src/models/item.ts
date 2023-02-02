05f209f3debdffeaf58c7740f75d72150183566e
rudnovd <rudnov.d@gmail.com>
2023-02-02 23:59:39 +0500

    feat(models): add new validators for item model

--- src/models/item.ts
+++ src/models/item.ts
@@ -1,4 +1,5 @@
 import { model, Schema } from 'mongoose'
+import { itemsIcons } from '../constants.js'
 
 export interface Item {
   _id: string
@@ -22,40 +23,82 @@ const schema = new Schema(
     icons: {
       type: Array,
       required: true,
-      validate: {
-        validator: (value: unknown) => Array.isArray(value) && value.length,
-        message: 'Icons must be not empty array',
-      },
+      validate: [
+        {
+          validator: (icons: Array<unknown>) => icons.length,
+          message: 'Icons must be not empty array',
+        },
+        {
+          validator: (icons: Array<unknown>) => {
+            for (const icon of icons) {
+              if (typeof icon !== 'string') {
+                return false
+              }
+            }
+          },
+          message: 'Icons elements must be strings',
+        },
+        {
+          validator: (value: Array<string>) => {
+            const icons: Array<string> = Object.keys(itemsIcons)
+              .map((group) => itemsIcons[group])
+              .flat()
+            for (const icon of value) {
+              if (!icons.includes(icon)) {
+                return false
+              }
+            }
+          },
+          message: 'Icons array contains not existing icon',
+        },
+      ],
     },
     images: {
       type: Array,
       default: [],
-      validate: {
-        validator: (value: unknown) => {
-          if (!Array.isArray(value)) {
-            return false
-          }
-          for (const url of value) {
-            if (process.env.IS_CLOUD_SERVER) {
-              if (url.indexOf('http://res.cloudinary.com/') !== 0) {
-                return false
-              }
-            } else {
-              if (url.indexOf('/upload/items/') !== 0) {
+      validate: [
+        {
+          validator: (images: Array<unknown>) => {
+            for (const image of images) {
+              if (typeof image !== 'string') {
                 return false
               }
             }
-          }
+          },
+          message: 'Images elements must be strings',
         },
-        message: 'Images must be array',
-      },
+        {
+          validator: (images: Array<string>) => {
+            for (const url of images) {
+              if (process.env.IS_CLOUD_SERVER) {
+                if (url.indexOf('http://res.cloudinary.com/') !== 0) {
+                  return false
+                }
+              } else {
+                if (url.indexOf('/upload/items/') !== 0) {
+                  return false
+                }
+              }
+            }
+          },
+          message: `Images array must starts with ${
+            process.env.IS_CLOUD_SERVER ? 'res.cloudinary.com' : '/upload/items'
+          } urls`,
+        },
+      ],
     },
     tags: {
       type: Array,
       default: [],
       validate: {
-        validator: (value: unknown) => Array.isArray(value),
-        message: 'Tags must be array',
+        validator: (tags: Array<unknown>) => {
+          for (const tag of tags) {
+            if (typeof tag !== 'string') {
+              return false
+            }
+          }
+        },
+        message: 'Tags elements must be strings',
       },
     },
     owner: {
